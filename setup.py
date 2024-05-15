0a64ea2e145a9c7ad3f0c6766d1bd700049ab5a4
Jurgen Buisman <jurgen@twodamdigital.nl>
2024-05-15 13:04:31 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='car_model_fixtures',
+    version='0.1.0',
+    description='Aims to add popular continental car models by continent and '
+                'year for realistic test data in the mobility sector',
+    url='https://github.com/Imperatus/car_model_fixtures',
+    author='Jurgen Buisman',
+    author_email='jurgen@twodamdigital.nl',
+    license='MIT',
+    packages=['car_model_fixtures'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Mobility',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+    ],
+)
